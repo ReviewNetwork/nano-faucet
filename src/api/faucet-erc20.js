d7d867140009caf8bc5ad4bb2dcff68e32799597
alan2207 <alan2207@live.com>
2018-09-03 15:23:38 +0200

    revert to prev commit

--- src/api/faucet-erc20.js
+++ src/api/faucet-erc20.js
@@ -1,63 +1,34 @@
-const config = require('@/config');
-const { validateEthAddress } = require('@services/validator');
-const erc20wallet = require('@services/erc20-wallet');
-const logger = require('@services/logger');
+const config = require('@/config')
+const { validateEthAddress } = require('@services/validator')
+const erc20wallet = require('@services/erc20-wallet')
+const logger = require('@services/logger')
 
 module.exports = {
-  async request(req, res) {
-    let { address } = req.params;
+  async request (req, res) {
+    let { address } = req.params
 
     if (!address || !validateEthAddress(address)) {
-      return res
-        .json({ error: 'Invalid Ethereum address', input: address })
-        .status(400);
+      return res.json({ error: 'Invalid Ethereum address', input: address }).status(400)
     }
 
-    const addressBalance = await erc20wallet.getBalanceOfAddress(address);
-
-    if (addressBalance >= config.ERC20_PAYOUT) {
-      console.log({ addressBalance });
-      return res
-        .json({
-          error: 'You already have enough tokens!',
-          balance: addressBalance,
-          input: address
-        })
-        .status(400);
-    }
-
-    logger(
-      `REQUESTED ERC20: ${config.ERC20_PAYOUT} ${
-        config.ERC20_NAME
-      } from ${address}`
-    );
+    logger(`REQUESTED ERC20: ${config.ERC20_PAYOUT} ${config.ERC20_NAME} from ${address}`)
 
     try {
-      await erc20wallet.sendErc20({ to: address, amount: config.ERC20_PAYOUT });
+      await erc20wallet.sendErc20({ to: address, amount: config.ERC20_PAYOUT })
 
-      let remainingBalance = await erc20wallet.getBalance();
+      let remainingBalance = await erc20wallet.getBalance()
 
-      logger(
-        `SENT ERC20: ${config.ETH_PAYOUT} ${config.ERC20_NAME} to ${address}`
-      );
-      logger(
-        `REMAINING ERC20: ${remainingBalance} ${
-          config.ERC20_NAME
-        } left in faucet`
-      );
-      return res.json({ amount: config.ERC20_PAYOUT, remainingBalance });
+      logger(`SENT ERC20: ${config.ETH_PAYOUT} ${config.ERC20_NAME} to ${address}`)
+      logger(`REMAINING ERC20: ${remainingBalance} ${config.ERC20_NAME} left in faucet`)
+      return res.json({ amount: config.ERC20_PAYOUT, remainingBalance })
     } catch (ex) {
-      logger(`ERROR: ${ex.message}`);
-      return res.json({ error: ex.message, input: address }).status(400);
+      logger(`ERROR: ${ex.message}`)
+      return res.json({ error: ex.message, input: address }).status(400)
     }
   },
 
   async getStatus(req, res) {
-    let balance = (await erc20wallet.getBalance()).toString();
-    return res.json({
-      balance,
-      address: erc20wallet.getAddress(),
-      currency: config.ERC20_NAME.toUpperCase()
-    });
+    let balance = (await erc20wallet.getBalance()).toString()
+    return res.json({ balance, address: erc20wallet.getAddress(), currency: config.ERC20_NAME.toUpperCase() })
   }
-};
+}
--- src/services/erc20-wallet.js
+++ src/services/erc20-wallet.js
@@ -2,37 +2,26 @@ const config = require('@/config')
 const web3 = require('@services/web3')
 const erc20 = require('@services/erc20')
 
-const wallet = web3.eth.accounts.privateKeyToAccount(
-  `0x${config.ERC20_PRIVATE_KEY}`
-)
+const wallet = web3.eth.accounts.privateKeyToAccount(`0x${config.ERC20_PRIVATE_KEY}`)
 
 const erc20Contract = erc20.contract
 
 module.exports = {
-  async sendErc20({ to, amount }) {
+  async sendErc20 ({ to, amount }) {
     const value = web3.utils.toBN(amount)
     return erc20.sendSigned(erc20Contract.methods.transfer(to, value))
   },
 
-  async getBalance() {
-    return erc20Contract.methods
-      .balanceOf(wallet.address)
-      .call()
+  async getBalance () {
+    return erc20Contract.methods.balanceOf(wallet.address).call()
       .then(balance => Number(balance))
   },
 
-  async getBalanceOfAddress(address) {
-    return erc20Contract.methods
-      .balanceOf(address)
-      .call()
-      .then(balance => Number(balance))
-  },
-
-  getAddress() {
+  getAddress () {
     return wallet.address
   },
 
-  getPrivateKey() {
+  getPrivateKey () {
     return wallet.privateKey
   }
 }
