41adffec5779bec976c23cfd30d2f2b8786a3dfb
alan2207 <alan2207@live.com>
2018-09-03 15:40:08 +0200

    limit funding

--- src/api/faucet-eth.js
+++ src/api/faucet-eth.js
@@ -1,34 +1,55 @@
-const config = require('@/config')
-const { validateEthAddress } = require('@services/validator')
-const wallet = require('@services/wallet')
-const logger = require('@services/logger')
+const config = require('@/config');
+const { validateEthAddress } = require('@services/validator');
+const wallet = require('@services/wallet');
+const logger = require('@services/logger');
 
 module.exports = {
-  async request (req, res) {
-    let { address } = req.params
+  async request(req, res) {
+    let { address } = req.params;
 
     if (!address || !validateEthAddress(address)) {
-      return res.json({ error: 'Invalid Ethereum address', input: address }).status(400)
+      return res
+        .json({ error: 'Invalid Ethereum address', input: address })
+        .status(400);
     }
 
-    logger(`REQUESTED: ${config.ETH_PAYOUT} ETH from ${address}`)
+    const addressBalance = await wallet.getBalanceOfAddress(address);
+
+    if (Number(addressBalance) >= config.ETH_PAYOUT) {
+      return res
+        .json({
+          error: 'You already have enough ETH!',
+          balance: addressBalance,
+          input: address
+        })
+        .status(400);
+    }
+
+    logger(`REQUESTED: ${config.ETH_PAYOUT} ETH from ${address}`);
 
     try {
-      let { transactionHash } = await wallet.sendEther({ to: address, amount: config.ETH_PAYOUT })
+      let { transactionHash } = await wallet.sendEther({
+        to: address,
+        amount: config.ETH_PAYOUT
+      });
 
-      let remainingBalance = await wallet.getBalance()
+      let remainingBalance = await wallet.getBalance();
 
-      logger(`SENT: ${config.ETH_PAYOUT} ETH to ${address}`)
-      logger(`REMAINING: ${remainingBalance} ETH left in faucet`)
-      return res.json({ transactionHash, amount: config.ETH_PAYOUT, remainingBalance })
+      logger(`SENT: ${config.ETH_PAYOUT} ETH to ${address}`);
+      logger(`REMAINING: ${remainingBalance} ETH left in faucet`);
+      return res.json({
+        transactionHash,
+        amount: config.ETH_PAYOUT,
+        remainingBalance
+      });
     } catch (ex) {
-      logger(`ERROR: ${ex.message}`)
-      return res.json({ error: ex.message, input: address }).status(400)
+      logger(`ERROR: ${ex.message}`);
+      return res.json({ error: ex.message, input: address }).status(400);
     }
   },
 
-  async getStatus (req, res) {
-    let balance = await wallet.getBalance()
-    return res.json({ balance, address: wallet.getAddress(), currency: 'ETH' })
+  async getStatus(req, res) {
+    let balance = await wallet.getBalance();
+    return res.json({ balance, address: wallet.getAddress(), currency: 'ETH' });
   }
-}
+};
--- src/services/erc20-wallet.js
+++ src/services/erc20-wallet.js
@@ -1,31 +1,42 @@
-const config = require('@/config')
-const web3 = require('@services/web3')
-const erc20 = require('@services/erc20')
+const config = require('@/config');
+const web3 = require('@services/web3');
+const erc20 = require('@services/erc20');
 
-const wallet = web3.eth.accounts.privateKeyToAccount(`0x${config.ERC20_PRIVATE_KEY}`)
+const wallet = web3.eth.accounts.privateKeyToAccount(
+  `0x${config.ERC20_PRIVATE_KEY}`
+);
 
-const erc20Contract = erc20.contract
+const erc20Contract = erc20.contract;
 
-const BN = web3.utils.BN
+const BN = web3.utils.BN;
 
 module.exports = {
-  async sendErc20 ({ to, amount }) {
-    const decimals = new BN(await erc20Contract.methods.decimals().call())
-    const ten = new BN(10, 10)
-    const value = new BN(amount, 10).mul(ten.pow(decimals))
-    return erc20.sendSigned(erc20Contract.methods.transfer(to, value))
+  async sendErc20({ to, amount }) {
+    const decimals = new BN(await erc20Contract.methods.decimals().call());
+    const ten = new BN(10, 10);
+    const value = new BN(amount, 10).mul(ten.pow(decimals));
+    return erc20.sendSigned(erc20Contract.methods.transfer(to, value));
   },
 
-  async getBalance () {
-    return erc20Contract.methods.balanceOf(wallet.address).call()
-      .then(balance => Number(balance))
+  async getBalance() {
+    return erc20Contract.methods
+      .balanceOf(wallet.address)
+      .call()
+      .then(balance => Number(balance));
   },
 
-  getAddress () {
-    return wallet.address
+  async getBalanceOfAddress(address) {
+    return erc20Contract.methods
+      .balanceOf(address)
+      .call()
+      .then(balance => Number(balance));
   },
 
-  getPrivateKey () {
-    return wallet.privateKey
+  getAddress() {
+    return wallet.address;
+  },
+
+  getPrivateKey() {
+    return wallet.privateKey;
   }
-}
+};
